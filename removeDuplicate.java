a9b3e14e18263703e2015fda7627d86a8a1ec909
Anokh406 <anokhkashyap@gmail.com>
2023-02-16 11:30:50 -0500

    remove duplicate  string

---
+++ removeDuplicate.java
@@ -0,0 +1,27 @@
+/**
+ * removeDuplicate
+ */
+public class removeDuplicate {
+    public static boolean[] map=new boolean[26];
+    
+    public static void removeDuplicates(String str,int idx,String newString){
+        if(idx==str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currentChar = str.charAt(idx);
+        if(map[currentChar -'a']){
+            removeDuplicates(str, idx+1, newString);
+        }else{
+            newString += currentChar;
+        map[currentChar-'a'] = true;
+        removeDuplicates(str, idx+1, newString);
+
+        }
+    }
+public static void main(String[] args) {
+    String str="abbccddddccccdfffda";
+    removeDuplicates(str,0, "");
+}
+    
+}
