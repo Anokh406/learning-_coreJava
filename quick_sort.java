d233e0c993d3ed180268c4ddabab5e3f501be417
Anokh406 <anokhkashyap@gmail.com>
2023-02-24 13:49:56 -0500

    Quick sort algorithm implemented

---
+++ quick_sort.java
@@ -0,0 +1,39 @@
+public class quick_sort {
+    public static int Partion_array(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+            }
+        }
+        // swap pivot
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
+    }
+
+    public static void quick_sorts(int arr[], int low, int high) {
+        if (low < high) {
+            int pivot = Partion_array(arr, low, high);
+            quick_sorts(arr, low, pivot - 1);
+            quick_sorts(arr, pivot + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        int n = arr.length;
+        quick_sorts(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + "");
+        }
+        System.out.println();
+    }
+}
