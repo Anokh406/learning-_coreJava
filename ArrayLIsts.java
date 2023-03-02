9eb34396ced8cca595b5bc968cfb23c671df7fad
Anokh406 <anokhkashyap@gmail.com>
2023-03-02 12:31:59 -0500

    Starting linkedList DSA

--- ArrayLIsts.java
+++ ArrayLIsts.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 public class ArrayLIsts {
@@ -10,20 +9,20 @@ public class ArrayLIsts {
         list.add(1);
         list.add(2);
         list.add(3);
-        // System.out.println(list);
+        System.out.println(list);
 
-        // // get elements into list
-        // int elements = list.get(0);
-        // System.out.println(elements);
+        // get elements into list
+        int elements = list.get(0);
+        System.out.println(elements);
         // set elements into list
-        // list.set(0, 10);
-        // System.out.println(list);
+        list.set(0, 10);
+        System.out.println(list);
         // delete elements from list
-        // list.remove(0);
-        // System.out.println(list);
+        list.remove(0);
+        System.out.println(list);
         // size of list
-        // int size = list.size();
-        // System.out.println(size);
+        int size = list.size();
+        System.out.println(size);
         // loops
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i));
--- backtrack.java
+++ backtrack.java
@@ -1,22 +1,25 @@
+import java.util.*;
+
 public class backtrack {
-   
-    public static void printPermutation(String str , int idx, String permutation ){
-        if(str.length()==0){
+
+    public static void printPermutation(String str, int idx, String permutation) {
+        if (str.length() == 0) {
             System.out.println(permutation);
             return;
         }
-        for(int i=0;i<str.length();i++){
+        for (int i = 0; i < str.length(); i++) {
             char currChar = str.charAt(i);
-            String newStr=str.substring(0,i)+str.substring(i+1);
-            printPermutation(newStr, idx+1, permutation+currChar);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            printPermutation(newStr, idx + 1, permutation + currChar);
         }
     }
+
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         permuteHelper(nums, new ArrayList<>(), result);
         return result;
     }
-    
+
     private void permuteHelper(int[] nums, List<Integer> permutation, List<List<Integer>> result) {
         if (permutation.size() == nums.length) {
             result.add(new ArrayList<>(permutation));
@@ -31,34 +34,35 @@ public class backtrack {
             }
         }
     }
+
     public static void main(String[] args) {
-        //  String str="abc";
-        //  printPermutation(str, 0,"");
-        int[] nums = {1,1,2,3,3,4,4,8,8};
+        // String str="abc";
+        // printPermutation(str, 0,"");
+        int[] nums = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
         permute(nums);
     }
-// leet code problem first solve
+    // leet code problem first solve
     // public int singleNonDuplicate(int[] nums) {
-    //     int left = 0;
-    //     int right = nums.length - 1;
-    //     while (left < right) {
-    //         int mid = (left + right) / 2;
-    //         if (mid % 2 == 1) {
-    //             // if mid is odd, move to the previous even index
-    //             mid--;
-    //         }
-    //         if (nums[mid] != nums[mid + 1]) {
-    //             // single element is to the left of mid
-    //             right = mid;
-    //         } else {
-    //             // single element is to the right of mid
-    //             left = mid + 2;
-    //         }
-    //     }
-    //     return nums[left];
+    // int left = 0;
+    // int right = nums.length - 1;
+    // while (left < right) {
+    // int mid = (left + right) / 2;
+    // if (mid % 2 == 1) {
+    // // if mid is odd, move to the previous even index
+    // mid--;
+    // }
+    // if (nums[mid] != nums[mid + 1]) {
+    // // single element is to the left of mid
+    // right = mid;
+    // } else {
+    // // single element is to the right of mid
+    // left = mid + 2;
+    // }
+    // }
+    // return nums[left];
     // }
     // public static void main(String[] args){
-    //     int[] nums = {1,1,2,3,3,4,4,8,8};
-    //     singleNonDuplicate(nums);
+    // int[] nums = {1,1,2,3,3,4,4,8,8};
+    // singleNonDuplicate(nums);
     // }
 }
